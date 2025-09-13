a3861eeacbc98f1807c8a33e3b78a9b01ec6368e
TheOneFoxAgo <nikitad2005@gmail.com>
2025-09-14 01:36:17 +0300

    Fixed the output

--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 	"log"
 	"math/big"
 	"math/bits"
+	"os"
 	"strings"
 )
 
-func parseFString(fString string) big.Int {
+func parseFString(l *log.Logger, fString string) big.Int {
 	f := big.Int{}
 	p, _ := f.SetString(fString, 2)
 	if p == nil {
-		log.Fatalln()
+		l.Fatalln()
 	}
 	return f
 }
 func main() {
+	l := log.New(os.Stderr, "zhiga: ", 0)
 	var fString, lString string
 	flag.StringVar(&fString, "f", "", "Values of function f. Delimeters are allowed. Example: 0101_0010")
 	flag.StringVar(&lString, "l", "", "Labels for variables. If provided, the output will be human-readable")
@@ -25,40 +27,39 @@ func main() {
 	fString = strings.ReplaceAll(fString, "_", "")
 	fLen := uint(len(fString))
 	if fLen < 2 {
-		log.Fatalln("Number of f values should be at least 2")
+		l.Fatalln("Number of f values should be at least 2")
 	}
 	if fLen > 256 {
-		log.Fatalln("Number of f values should be at most 256")
+		l.Fatalln("Number of f values should be at most 256")
 	}
 	if bits.OnesCount(fLen) != 1 {
-		log.Fatalln("Number of f values should be power of 2")
+		l.Fatalln("Number of f values should be power of 2")
 	}
-	labelsAmount := bits.LeadingZeros(fLen)
 	if lString == "" {
-		res := zhigalkin(parseFString(fString), fLen)
-		fmt.Println(res.Text(2))
+		res := zhigalkin(parseFString(l, fString), fLen)
+		fmt.Printf("%0*s\n", fLen, res.Text(2))
 	} else {
+		labelsAmount := bits.TrailingZeros(fLen)
 		if labelsAmount > len(lString) {
-			log.Fatalln("Not enough labels")
+			l.Fatalln("Not enough labels")
 		}
 		for i, c := range lString {
 			if i >= labelsAmount {
 				break
 			}
-			fmt.Print(c, " ")
+			fmt.Printf("%c ", c)
 		}
-		fmt.Println("Calculating...")
-		res := zhigalkin(parseFString(fString), fLen)
+		res := zhigalkin(parseFString(l, fString), fLen)
 		fmt.Println("| Res:")
-		for combination := 0; combination < int(fLen); combination += 1 {
-			for mask := 1; mask < int(fLen); mask <<= 1 {
+		for combination := uint(0); combination < fLen; combination += 1 {
+			for mask := fLen >> 1; mask > 0; mask >>= 1 {
 				if combination&mask == 0 {
 					fmt.Print("0 ")
 				} else {
 					fmt.Print("1 ")
 				}
 			}
-			fmt.Printf("|  %b\n", res.Bit(combination))
+			fmt.Printf("|  %1b\n", res.Bit(int(combination)))
 		}
 	}
 }
